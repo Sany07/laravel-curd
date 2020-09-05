c977c55736858dc2ac8c34b6042095d722c952cf
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-09-05 23:45:22 +0600

    first commit

---
+++ resources/views/contacts/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+ <div class="col-sm-8 offset-sm-2">
+  <div>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="POST" action="{{ route('contacts.update' , $contact->id)  }}">
+           @method('PATCH') 
+            @csrf
+          <div class="form-group">    
+              <label for="first_name">First Name:</label>
+              <input type="text" class="form-control" name="first_name" value="{{ $contact->first_name }}"/>
+          </div>
+
+          <div class="form-group">
+              <label for="last_name">Last Name:</label>
+              <input type="text" class="form-control" name="last_name" value="{{ $contact->last_name }}"/>
+          </div>
+
+          <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="text" class="form-control" name="email" value="{{ $contact->email }}"/>
+          </div>
+          <div class="form-group">
+              <label for="city">City:</label>
+              <input type="text" class="form-control" name="city" value="{{ $contact->city }}"/>
+          </div>
+          <div class="form-group">
+              <label for="country">Country:</label>
+              <input type="text" class="form-control" name="country" value="{{ $contact->country }}"/>
+          </div>
+                       
+          <button type="submit" class="btn btn-primary">Updated</button>
+          <br>
+      </form>
+  </div>
+</div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+<div class="col-sm-12">
+
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>City</td>
+          <td>Country</td>
+          <td colspan = 2>Actions</td>
+        </tr>
+    </thead>
+    
+    <tbody>
+        @foreach($contacts as $contact)
+        <tr>
+            <td>{{$contact->id}}</td>
+            <td>{{$contact->first_name}} {{$contact->last_name}}</td>
+            <td>{{$contact->email}}</td>
+            <td>{{$contact->city}}</td>
+            <td>{{$contact->country}}</td>
+            <td>
+            <a href="{{ route('contacts.edit',$contact->id)}}" class="btn btn-primary">Edit</a>
+            </td>
+
+            <td>
+                <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+</div>
+@endsection
